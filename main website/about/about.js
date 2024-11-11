a3f169307809bb0dc5531bc778fd1025d304a5d9
SARTHAK2521 <156179259+SARTHAK2521@users.noreply.github.com>
2024-11-11 09:34:08 +0530

    Delete main website directory

--- main website/about/about.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.team-member').forEach(member => {
-    member.addEventListener('click', () => {
-        const memberName = member.getAttribute('data-member');
-        alert(`You clicked on ${memberName}`);
-    });
-});
